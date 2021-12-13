ee8f6ea25612db71fbbb21af00ebeac51ec66aa7
Zachary Wright <80498861+wrightzachary@users.noreply.github.com>
2021-12-13 10:20:57 -0600

    Adding React Icons and main body text

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,35 @@
 import React from 'react';
 import './App.css';
 // import VideoPlayer from './components/VideoPlayer/VideoPlayer.jsx';
-import TopBar from './components/TopBar/TopBar.jsx';
-import {Container} from '@chakra-ui/react';
+import {Container, Button} from '@chakra-ui/react';
 import ReactPlayer from 'react-player';
+import {FaDiscord, FaTwitter, FaTwitch , FaYoutube} from 'react-icons/fa'
 
 function App() {
   return (
     <Container maxW='container.xl' className="app">
-      <TopBar />
+
+      {/* Subscribe Button and YouTube icon*/}
+      <Button className="subscribe-cta">Subscribe</Button> <FaYoutube size={50} />
+
+      {/* Main Content */}
       <section className ='video-area'>
         <h1 className="text-logo">codeFellowship( )</h1> 
 
+        {/* Video Player */}
         <ReactPlayer className='videopodcast-player'
         url= 'https://youtu.be/_lAI4n6rODE' controls width='80%' height='80vh'/>
 
+        {/* icons */}
+        <div className='icons'>
+          <FaDiscord size={35} /> <FaTwitter size={40} /> <FaTwitch size={35} />
+        </div>
+
+        {/* Main Body Text */}
+        <h1 className='text-body'>// FULL SITE COMING SOON</h1>
+
       </section>
+      
 
       {/* <VideoPlayer /> */}
     </Container>
