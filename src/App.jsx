9d0a4d2c1566ec90c506a78d67940baf7c5396f5
Joshua Berrios <ixoyedesignstudio@gmail.com>
2022-01-06 02:07:21 +0000

    Fixed white border issue around application by add a 0 margin to the body styling in the index.html file + Refactoring App

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,41 @@
 import React from 'react';
 import './App.css';
-import {Container, Button} from '@chakra-ui/react';
-import ReactPlayer from 'react-player';
 import {FaDiscord, FaTwitter, FaTwitch} from 'react-icons/fa'
 
 function App() {
   return (
-    <Container maxW='container.xl' className="app">
+    // <div className="app">
 
-      {/* Subscribe Button*/}
-      <Button className="subscribe-cta">Subscribe</Button> 
+    //   {/* Subscribe Button*/}
+    //   <Button className="subscribe-cta">Subscribe</Button> 
 
-      {/* Main Content */}
-      <section className ='video-area'>
-        <h1 className="text-logo">codeFellowship( )</h1> 
+    //   {/* Main Content */}
+    //   <section className ='video-area'>
+    //     <h1 className="text-logo">codeFellowship( )</h1> 
 
-        {/* Video Player */}
-        <div className='player'>
-          <ReactPlayer className='videopodcast-player'
-          url= 'https://youtu.be/_lAI4n6rODE' controls width='80%' height='80vh'/>
-        </div>
+    //     {/* Video Player */}
+    //     <div className='player'>
+    //       <ReactPlayer className='videopodcast-player'
+    //       url= 'https://youtu.be/_lAI4n6rODE' controls width='80%' height='80vh'/>
+    //     </div>
 
-        {/* icons */}
-        <div className='icons'>
-          <FaDiscord size={35} /> <FaTwitter size={40} /> <FaTwitch size={35} />
-        </div>
+    //     {/* icons */}
+    //     <div className='icons'>
+    //       <FaDiscord size={35} /> <FaTwitter size={40} /> <FaTwitch size={35} />
+    //     </div>
 
-        {/* Main Body Text */}
-        <h1 className='text-body'>// FULL SITE COMING SOON</h1>
+    //     {/* Main Body Text */}
+    //     <h1 className='text-body'>// FULL SITE COMING SOON</h1>
 
-        {/* copyright */}
-        <div className='copyright' >
-          <p>© 2021 - codeFellowship( ) &#123; the podcast &#125;</p>
-        </div>
-      </section>
-    </Container>
+    //     {/* copyright */}
+    //     <div className='copyright' >
+    //       <p>© 2021 - codeFellowship( ) &#123; the podcast &#125;</p>
+    //     </div>
+    //   </section>
+    // </div>
+    <div className="refactored">
+      Refactoring...
+    </div>
   );
 }
 
